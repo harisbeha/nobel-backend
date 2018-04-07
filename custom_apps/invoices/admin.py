0f70564ecf96ff5bff43a385c88e408504c66d47
Madison Steiner <madison.is@hopeful.af>
2018-04-07 15:07:27 -0700

    Implement invoice admin

--- custom_apps/invoices/admin.py
+++ custom_apps/invoices/admin.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Invoice
+
+
+class CustomModelAdmin(admin.ModelAdmin):
+
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
+        super(CustomModelAdmin, self).__init__(model, admin_site)
+
+
+class InvoiceAdmin(CustomModelAdmin): pass
+
+
+admin.site.register(Invoice, InvoiceAdmin)
