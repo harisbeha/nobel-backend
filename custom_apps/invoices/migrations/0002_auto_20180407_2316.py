8d4479d73c2df41c12749e60436e9dbdf7eb4b4a
Audrey Dutcher <audrey@rhelmot.io>
2018-04-07 16:53:05 -0700

    Change migration shitfuck to be id 1 (NULLBUILDING in "prod")

--- custom_apps/invoices/migrations/0002_auto_20180407_2316.py
+++ custom_apps/invoices/migrations/0002_auto_20180407_2316.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='workorder',
             name='building',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='invoices.Building'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='invoices.Building'),
             preserve_default=False,
         ),
         migrations.AddField(
