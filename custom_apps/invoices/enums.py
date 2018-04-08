1f88d15cf739a84b4306adc8952a34fa57933f6a
Audrey Dutcher <audrey@rhelmot.io>
2018-04-08 02:08:36 -0700

    evil shadowing bug

--- custom_apps/invoices/enums.py
+++ custom_apps/invoices/enums.py
@@ -8,7 +8,7 @@ class ChoiceEnum(Enum):
         return tuple((x.value, x.name) for x in cls)
 
     @classmethod
-    def name(cls, value):
+    def text_name(cls, value):
         try:
             return next(x.name for x in cls if x.value == value)
         except StopIteration:
@@ -16,9 +16,10 @@ class ChoiceEnum(Enum):
 
     @classmethod
     def human_name(cls, value):
-        inhuman = cls.name(value)
-        lowercase =' '.join(x.lower() for x in inhuman.split('_'))
+        inhuman = cls.text_name(value)
+        lowercase = ' '.join(x.lower() for x in inhuman.split('_'))
         return lowercase[0].upper() + lowercase[1:]
+
 
 class ReportState(ChoiceEnum):
     CREATED = 1
