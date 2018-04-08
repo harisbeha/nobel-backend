d230ff0ebf9596376b970b6826d4032ba2e5df63
Madison Steiner <madison.is@hopeful.af>
2018-04-07 20:59:35 -0700

    Create reportstate enum

---
+++ custom_apps/invoices/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ReportState(Enum):
+    CREATED = 1
+    INITIALIZED = 2
+    SAFETY_REVIEWED = 3
+    SAFETY_REVIEW_CLOSED = 4
+    PRELIM_GENERATED = 5
+    PRE_FORECAST_REVIEWED = 6
+    FORECASTED = 7
+    SENT_TO_PROVIDER = 8
+    VALIDATED = 9
+    FINALIZED = 10
+
+    REJECTED = -1
