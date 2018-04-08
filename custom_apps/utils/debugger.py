e264471012b712cf823c80e27d7bed08f8f8e6ee
Madison Steiner <madison.is@hopeful.af>
2018-04-08 00:07:26 -0700

    Implement debugger

--- custom_apps/utils/debugger.py
+++ custom_apps/utils/debugger.py
@@ -3,6 +3,6 @@ def setup():
         import googleclouddebugger
         googleclouddebugger.enable(version='v0.0.1', module='backend', project_id='nobel-weather-associates',
                                    project_number='419191605818',
-                                   service_account_json_file='/app/google-cloud.json')
+                                   service_account_json_file='/app/google-cloud.json', enable_service_account_auth=True)
     except ImportError:
         pass
