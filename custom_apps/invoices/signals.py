d778993c3c9b5561e995d7a90b474ef5340b921e
FTI Dev <ops+devs@futuretech.industries>
2018-04-30 22:38:24 -0400

    Add base pre_save signal for user

--- custom_apps/invoices/signals.py
+++ custom_apps/invoices/signals.py
@@ -1,7 +1,14 @@
 from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+# replace with shortcut get grab auth model
 
 from .models import *
 
 
 def connect_state_workflow():
     pass
+
+@receiver(pre_save, sender=User)
+def set_email_to_username(sender, instance, **kwargs):
+    user.email = instance.username
