79a567d227dc585c5985b35926a5cdf5fc5e405c
FTI Dev <ops+devs@futuretech.industries>
2018-04-30 22:42:31 -0400

    Fix formatting and import all package-level models

--- custom_apps/invoices/resources.py
+++ custom_apps/invoices/resources.py
@@ -1,5 +1,5 @@
 from import_export import resources
-from custom_apps.invoices.models import Building
+from .models import *
 
 """
 Usage
@@ -9,82 +9,99 @@ dataset = WorkOrderResource().export()
 print dataset.csv 
 result
 """
+
+
 class AddressMetadataStorageMixinResource(resources.ModelResource):
     class Meta:
         model = AddressMetadataStorageMixin
-		fields = '__all__'
+        fields = '__all__'
+
 
 class VendorSettingsResource(resources.ModelResource):
     class Meta:
         model = VendorSettings
-		fields = '__all__'
+        fields = '__all__'
+
 
 class VendorResource(resources.ModelResource):
     class Meta:
         model = Vendor
-		fields = '__all__'
+        fields = '__all__'
 
-class  RegionalAdminResource(resources.ModelResource):
+
+class RegionalAdminResource(resources.ModelResource):
     class Meta:
         model = RegionalAdmin
-		fields = '__all__'
+        fields = '__all__'
+
 
 class InvoiceResource(resources.ModelResource):
     class Meta:
         model = Invoice
-		fields = '__all__'
+        fields = '__all__'
+
 
 class BuildingManagerResource(resources.ModelResource):
     class Meta:
         model = BuildingManager
-	fields = '__all__'
+    fields = '__all__'
+
 
 class BuildingResource(resources.ModelResource):
     class Meta:
         model = Building
-		fields = '__all__'
+        fields = '__all__'
+
 
 class WorkOrderResource(resources.ModelResource):
     class Meta:
         model = WorkOrder
-		fields = '__all__'
+        fields = '__all__'
+
 
 class WorkVisitResource(resources.ModelResource):
     class Meta:
         model = WorkVisit
-		fields = '__all__'
+        fields = '__all__'
+
 
 class SafetyReportResource(resources.ModelResource):
     class Meta:
         model = SafetyReport
-		fields = '__all__'
+        fields = '__all__'
+
 
 class DiscrepancyReportResource(resources.ModelResource):
     class Meta:
         model = DiscrepancyReport
-		fields = '__all__'
+        fields = '__all__'
+
 
 class RegionalAdminProxyNWAResource(resources.ModelResource):
     class Meta:
         model = RegionalAdminProxyNWA
-		fields = '__all__'
+        fields = '__all__'
+
 
 class WorkOrderProxyNWAResource(resources.ModelResource):
     class Meta:
-		        model = WorkOrderProxyNWA
-fields = '__all__'
+        model = WorkOrderProxyNWA
+        fields = '__all__'
+
 
 class VendorProxyCBREResource(resources.ModelResource):
     class Meta:
         model = VendorProxyCBRE
-		fields = '__all__'
+        fields = '__all__'
+
 
 class WorkOrderProxyCBREResource(resources.ModelResource):
     class Meta:
         model = WorkOrderProxyCBRE
-		fields = '__all__'
+        fields = '__all__'
+
 
 class WorkOrderProxyVendorResource(resources.ModelResource):
     class Meta:
         model = WorkOrderProxyVendor
-		fields = '__all__'
+        fields = '__all__'
