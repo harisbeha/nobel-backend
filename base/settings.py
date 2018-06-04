d0e83971be9abd2c4fa76e566e9455e6133e3021
FTI Dev <ops+devs@futuretech.industries>
2018-06-04 07:52:19 -0400

    template loaders

--- base/settings.py
+++ base/settings.py
@@ -80,6 +80,13 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
 WSGI_APPLICATION = 'base.wsgi.application'
 
 # Database
